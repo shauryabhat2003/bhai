4feba4254b49c306bb9b1b476781c235055220d7
shauryabhat2003 <shauryabhatnagar1511@gmail.com>
2025-01-30 22:48:53 +0530

    Cors second Commit

--- bhai-backend/index.js
+++ bhai-backend/index.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(cors({
     origin: process.env.CLIENT_URL.trim(),
     credentials: true,
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
 }));
 
 app.use(express.json());
