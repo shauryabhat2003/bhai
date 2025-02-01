efbd024fd38df594cc2472efb902db241da981ed
shauryabhat2003 <shauryabhatnagar1511@gmail.com>
2025-02-01 12:07:51 +0530

    Update-2 01 Feb 2025

--- bhai-backend/index.js
+++ bhai-backend/index.js
@@ -19,6 +19,8 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
   })
 );
 
